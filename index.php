ba0ff9a6ea197e0514172374675bd8950e27b41f
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-14 13:58:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+try{
+$pdo=new PDO("mysql:host=localhost;dbname=dbname",'root','');
+echo 'you are connected';
+}catch(PDOException $e){
+    echo 'you hace error'.$e->getMessage();
+}
+
+
+?>
